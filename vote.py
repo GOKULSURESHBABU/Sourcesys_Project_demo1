7e3846db767bfd12c3c0f346cb7804d4b143c35a
GOKULSURESHBABU <gokulsureshbabu2004@gmail.com>
2026-03-26 10:14:54 +0000

    first commit

--- vote.py
+++ vote.py
@@ -4,3 +4,11 @@ if age >= 18:
     print("Eligible to vote")
 else:
     print("Not eligible to vote")
+
+#odd or even program 
+n = int(input("Enter number: "))
+
+if n % 2 == 0:
+    print("Even")
+else:
+    print("Odd")
